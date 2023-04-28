4db6d73a18bd67e182b1572025e59bd970b4e6a7
Michael Narváez <michaelnarvaez@Michaels-MacBook-Air.local>
2023-04-28 17:23:37 -0600

    ADD: labo6 done

---
+++ Laboratorio6_Repository-ViewModel/app/src/main/java/com/example/laboratorio5_navigationcomponent2/NewMovieFragment.kt
@@ -0,0 +1,25 @@
+package com.example.laboratorio5_navigationcomponent2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class CreateMovieFragment : Fragment() {
+    private lateinit var actionSubmit: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        actionSubmit = view.findViewById(R.id.actio_submit)
+    }
+}
